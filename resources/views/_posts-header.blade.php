82f4055228df6a8c24189dd36f018bddd7e974b8
KhineZinThaw <khinezinthaw71@gmail.com>
2021-10-29 07:31:28 +0630

    section 6, ep7, episode 39 | Advanced Eloquent Query

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -36,7 +36,7 @@
                 <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display: none">
                     <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:rext-white focus:text-white">All</a>
                     @foreach ($categories as $category)
-                        <a href="/categories/{{ $category->slug }}" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:rext-white focus:text-white {{ isset($currentCategory) && $currentCategory->is($category)? 'bg-blue-500 text-white' : '' }}">{{ $category->name }}</a>
+                        <a href="/?categories={{ $category->slug }}" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:rext-white focus:text-white {{ isset($currentCategory) && $currentCategory->is($category)? 'bg-blue-500 text-white' : '' }}">{{ $category->name }}</a>
                     @endforeach
                 </div>
             </div>      
