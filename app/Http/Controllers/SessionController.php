a44c599157290b662b045b265e3b7f2deddf9089
KhineZinThaw <khinezinthaw71@gmail.com>
2021-11-04 17:26:41 +0630

    section 9, episode 50 | Build the login page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,10 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        //validate the request
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        //based on provided crenditials
+        if(auth()->attempt($attributes)) {
+            //session fixation
+            session()->regenerate();
+            
+            //redirect
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        //auth failed
+        throw ValidationException::withMessages([
+            'email' => 'Your email could not be verified'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
